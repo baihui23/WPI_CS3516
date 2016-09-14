55805712b688c57647545fe4e4896b768cb432d2
Jules Voltaire <javoltaire@wpi.edu>
2016-09-14 14:34:42 -0400

    http client now accepts the correct number of arguments, visual studio code folder added to git ignore

--- proj1/proj1_work/http_client.c
+++ proj1/proj1_work/http_client.c
@@ -1,8 +1,37 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <string.h>
 
+// Constants
+#define P_OPTION "-p"
+
+// Function prototypes
+void showHelp();                                // Prints a series messages on how to use this program
+
+/**
+ * This main method is the starting point of this program, it takes a number of arguments which will be used as options, url and port number;
+ The address  and port number are required arguments.
+ */
 int main(int argc, char *argv[]){
-    printf("Hello World!\n");
+    int error_result = 0;                       // int to return as a result for this function;
 
-    return 0;
+    // Checking to make sure that we have enough arguments
+    if(argc < 4){
+        printf("ERROR: You need give a url and and a port number.\n");
+        showHelp();
+        error_result = 1;
+    }
+    else{
+
+    }
+    return error_result;
+}
+
+/**
+ * Prints messages on how to use this program to the screen.
+ */
+void showHelp(){
+    printf("USAGE: ./http_client [-options] server_url port_number\n");
+    printf("Avalaible options:\n");
+    printf("-p: Prints the RTT for accessing the URL before the server's response.\n");
 }
